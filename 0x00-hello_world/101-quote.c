251a7c5e8b20086c9d0cecd48631b492188060d0
David M <117746069+Muth80@users.noreply.github.com>
2022-12-08 19:19:39 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,11 @@
 /**
  * main - Entry Point
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
-
 }
