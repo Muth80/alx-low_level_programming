66929cf17e5ab30ff727f350e78934ef021cf747
David Muindi <muindidiego@gmail.com>
2022-12-08 06:07:21 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+
+
 /**
+
 *main - Entry Point
+
 *
+
 *Return: Always 0 (Sucess)
+
 */
 
+
+
 int main(void)
+
 {
+
     puts("\"Programming is like building a multilingual puzzle");
+
     return (0);
+
     
+
 }
