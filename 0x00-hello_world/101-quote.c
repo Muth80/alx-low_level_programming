c235310934a0ce0a876853fc3343392ef7acc856
David M <117746069+Muth80@users.noreply.github.com>
2022-12-08 19:17:46 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - prints and that piece of art is useful
+ * main - Entry Point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
