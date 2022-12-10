619d8711694cbb06e64eb7a37eb9023ed155e4a4
David Muindi <muindidiego@gmail.com>
2022-12-10 06:15:01 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		} else
+		{
+			putchar(x);
+			x++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
