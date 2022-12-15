f8a855839bb69d60c578d575eb5ef369fb38aced
David M <117746069+Muth80@users.noreply.github.com>
2022-12-15 23:36:04 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,8 +9,5 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 529016697fcfe6360669d9e022d495bc4423ebd2
 #endif 
