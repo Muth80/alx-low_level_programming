85948df0ea721b8e3782b030d92ce510a35379a1
David Muindi <muindidiego@gmail.com>
2023-01-05 09:48:53 -0800

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,37 @@
 #include "main.h"
 
+int prime_number(int n);
+int divisible(int a, int b);
+int power(int x, int y);
+
 /**
- * is_divisible - Checks if a number is divisible
- * @num: The number to be checked.
- * @div: The divisor
- * @n: The function will check if the integer is a prime number
- * and will return 0 if it is not, or 1 if it is
- * Return: If the number is divisble - 0.
- * If the number is not divisible -1.
+ * prime_number - checks if an integer is prime or not
+ * @n: number to be checked
+ * Return: 1 if prime, 0 otherwise
  */
 
-int is_prime_number(int n)
+int prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
-	{
-		_putchar(0 + '0');
 		return (0);
-	}
-
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-		{
-			_putchar(0 + '0');
-			return (0);
-		}
-	}
-
-	_putchar(1 + '0');
-	return (1);
+	if (n == 2 || n == 3)
+		return (1);
+	if (divisible(n, 2) || divisible(n, 3))
+		return (0);
+	if (power(5, (n / 5)) > n)
+		return (1);
+	return (0);
 }
+
+/**
+ * divisible - checks if a is divisible by b
+ * @a: dividend
+ * @b: divisor
+ * Return: 1 if divisible, 0 otherwise
+ */
+
+int divisible(int a, int b)
+{
+	if (a % b == 0)
+		return (1);
 
