a35994ede14ed355cd0e3b5b99ff1693d1c8ba71
David Muindi <muindidiego@gmail.com>
2023-01-05 03:13:57 -0800

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,15 +7,11 @@
  * Return: evaluate sqrt
  */
 
-int _evaluate(int i, int n)
+int _sqrt_recursion(int n);
 {
-	if (n == 0 || n == 1)
-		return (n);
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
-	else if (i * i == n)
-		return (i);
-	return (-1);
-	return (-1);
+	if (n < 0)
+		return (-1);
+	else
+		return (helperFunction(n, 0));
 }
 
