d1264b615e0deed225c6f8a2309e5e1e7e3e9440
David Muindi <muindidiego@gmail.com>
2023-01-05 03:37:41 -0800

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_divisible - Checks if a number is divisible
+ * @num: The number to be checked.
+ * @div: The divisor.
+ * Return: If the number is divisble - 0.
+ * If the number is not divisible -1.
+ */
+
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+}
