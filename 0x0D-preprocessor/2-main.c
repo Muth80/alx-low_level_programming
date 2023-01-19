1124f16e9af784cf816edd89b689687782aa374b
David Muindi <muindidiego@gmail.com>
2023-01-19 03:22:38 -0800

    Task 3

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,12 @@
 
 /**
  * main - prints the name of the file program was compiled from
+ * _FILE_ -macro is used to print the current file name
  * Return: Always 0
  */
 
 int main(void)
 {
 	printf("%s\n", _FILE_);
-	return (0);
+	return 0;
 }
